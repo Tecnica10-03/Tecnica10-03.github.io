3a6de036b2317d64d34aee74a209a6bef9a445ab
Marles27 <jorgeandresmarlesflorez16.708@gmail.com>
2020-09-19 17:11:12 -0500

    alerta bacanita

--- alert.js
+++ alert.js
@@ -1,1 +1,9 @@
-confirm("Anda joya la pagina sioke");
+function alerta() {
+    var pregunta = confirm("Anda joya la pagina sioke");
+
+    if(pregunta){
+        alert("Bre grx");
+    }else{
+        alert("Coma monda entonces :)");
+    }
+}
